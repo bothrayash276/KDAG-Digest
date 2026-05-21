2d7fa117253d77f37d70ffe5bba21543b695cbc8
bothrayash276 <bothrayash276@gmail.com>
2026-05-21 16:19:56 +0530

    completed the laptop desgin of header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,30 +4,46 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <>
-      {/* Header Container */}
+      {/* Header Container*/}
       <div
-      className='w-full flex items-center'>
+      className='w-full flex items-center gap-3 sm:justify-between'>
 
         {/* Logo */}
         <img 
         src="logo.png" 
         alt="The KDAG Digest"
-        className='h-10'
+        className='h-10 not-sm:hidden'
          />
 
+        <img src="icon.png" alt="The KDAG Digest" 
+        className='h-10 sm:hidden'/>
+
         {/* Search Bar */}
-        <input 
-        type="text" 
-        className='flex-2 bg-none rounded-4xl outline-none border border-neutral-400 mx-5 text-white placeholder-neutral-400 px-8 h-12'
-        placeholder='Search'
-        />
+        <div className='flex-1 gap-5 border-2 border-[#333333] bg-[#1a1a1a] h-12 flex items-center px-5 py-6 rounded-xl sm:max-w-[50vw]'>
+          <img src="/search.svg" alt="" className='h-5' />
+          
+          <input type="text" placeholder='Search' 
+          className='placeholder:text-white flex-1 outline-none text-white'/>
+        </div>
+
+        
+      {/* Buttons */}
+      <div
+      className='flex gap-8 justify-start not-sm:hidden'>
+          {/* Home Button */}
+        <Link 
+        to={'/'}
+        className='text-white hover:text-[#E92F30]'>
+        Home
+        </Link>
 
         {/* Blog Button */}
         <Link 
         to={'/blogs'}
-        className='text-[#E92F30] border-2 p-3 px-6 rounded-xl hover:text-white hover:bg-[#E92F30] hover:border-[#E92F30] font-[Space_Mono]'>
+        className='text-white hover:text-[#E92F30]'>
         Blogs
         </Link>
+      </div>
 
       </div>
     </>
