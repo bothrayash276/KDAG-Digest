f6c8475413879f0096aa0a1021dd296395e4ad09
bothrayash276 <bothrayash276@gmail.com>
2026-05-23 10:53:54 +0530

    loading spinner setted up

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import ClassicSpinner from './Components/Loading'
+import { useEffect, useState } from 'react'
 
 const Blogs = () => {
+
+  const [loading, setLoading] = useState(true)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const fetchBlogs = await fetch('http://localhost:3000/posts');
+        setBlogs(fetchBlogs);
+      } catch (e) {
+        console.log(e);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+    getBlogs()
+    
+  }, [])
+  
+
+
+  if(loading) return (
+    <>
+    <ClassicSpinner />
+    </>
+  )
+
   return (
-    <div>
+    <>
+    {/* Body Container */}
+    <div
+    className='h-full w-full flex sm:flex-row not-sm:flex-col'>
       
     </div>
+    </>
   )
 }
 
